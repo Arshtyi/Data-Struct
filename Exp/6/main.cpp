cd1fe3bf832d3369914957269586b732dc358792
Arshtyi <arshtyi@foxmail.com>
2025-10-23 11:31:31 +0800

    Exp 6

--- Exp/6/main.cpp
+++ Exp/6/main.cpp
@@ -28,7 +28,7 @@ public:
     void pop()
     {
         if (stackTop == -1)
-            exit(0);
+            throw out_of_range("Stack is empty");
         stack[stackTop--].~T();
     }
     void push(const T &theElement)
@@ -137,7 +137,7 @@ double Calculate_Exp(arrayStack<char> &exp_stack)
 const int N = 102, M = 2005;
 int n;
 char ex[M];
-signed main()
+void run_expression()
 {
     cin >> n;
     for (int i = 1; i <= n; ++i)
@@ -147,5 +147,148 @@ signed main()
         Suffix_Exp(ex, exp_stack);
         cout << fixed << setprecision(2) << Calculate_Exp(exp_stack) << endl;
     }
+}
+struct NodePos
+{
+    int r, c, dir;
+};
+const int MAXMZ = 256, dr[] = {0, 1, 0, -1}, dc[] = {1, 0, -1, 0};
+int r, c, maze[MAXMZ][MAXMZ], pathMark[MAXMZ][MAXMZ], vis[MAXMZ][MAXMZ], pr[MAXMZ * MAXMZ], pc[MAXMZ * MAXMZ], vis2[MAXMZ][MAXMZ];
+bool load_maze()
+{
+    freopen("maze.txt", "r", stdin);
+    cin >> r >> c;
+    for (int i = 0; i < r; ++i)
+        for (int j = 0; j < c; ++j)
+            cin >> maze[i][j];
+    return r > 0 && c > 0;
+}
+bool solve_maze()
+{
+    if (maze[0][0] || maze[r - 1][c - 1])
+        return false;
+    arrayStack<NodePos> st(r * c + 10);
+    NodePos start{0, 0, 0};
+    st.push(start);
+    vis[0][0] = 1;
+    bool found = false;
+    while (!st.empty())
+    {
+        NodePos &cur = st.top();
+        if (cur.r == r - 1 && cur.c == c - 1)
+        {
+            found = true;
+            break;
+        }
+        if (cur.dir >= 4)
+        {
+            st.pop();
+            continue;
+        }
+        int dir = cur.dir;
+        cur.dir++;
+        int nr = cur.r + dr[dir], nc = cur.c + dc[dir];
+        if (nr >= 0 && nr < r && nc >= 0 && nc < c && !vis[nr][nc] && !maze[nr][nc])
+        {
+            vis[nr][nc] = 1;
+            NodePos nxt{nr, nc, 0};
+            st.push(nxt);
+        }
+    }
+    if (!found)
+        return false;
+    arrayStack<NodePos> tmp;
+    while (!st.empty())
+    {
+        tmp.push(st.top());
+        st.pop();
+    }
+    while (!tmp.empty())
+    {
+        NodePos p = tmp.top();
+        tmp.pop();
+        pathMark[p.r][p.c] = 1;
+        st.push(p);
+    }
+    return true;
+}
+void print_path()
+{
+    int maxLen = r * c, len = 0, x = 0, y = 0;
+    pr[len] = x, pc[len] = y;
+    len++;
+    vis2[x][y] = 1;
+    while (!(x == r - 1 && y == c - 1) && len < maxLen)
+    {
+        bool adv = false;
+        for (int k = 0; k < 4; ++k)
+        {
+            int nr = x + dr[k];
+            int nc = y + dc[k];
+            if (nr >= 0 && nr < r && nc >= 0 && nc < c && pathMark[nr][nc] && !vis2[nr][nc])
+            {
+                x = nr, y = nc;
+                vis2[x][y] = 1;
+                pr[len] = x, pc[len] = y;
+                len++;
+                adv = true;
+                break;
+            }
+        }
+    }
+    printf("Path length: %d\n", len);
+    printf("Path: ");
+    for (int i = 0; i < len; ++i)
+    {
+        printf("(%d,%d)%s", pr[i] + 1, pc[i] + 1, (i + 1 < len ? "->" : "\n"));
+    }
+    printf("\nMap with path (*):\n");
+    for (int i = 0; i < r; ++i)
+    {
+        for (int j = 0; j < c; ++j)
+        {
+            char ch;
+            if (maze[i][j] == 1)
+                ch = '#';
+            else if (pathMark[i][j])
+                ch = '*';
+            else
+                ch = '.';
+            putchar(ch);
+            if (j + 1 < c)
+                putchar(' ');
+        }
+        putchar('\n');
+    }
+}
+void run_maze()
+{
+    if (!load_maze())
+    {
+        return;
+    }
+    if (!solve_maze())
+    {
+        printf("No path from (1,1) to (%d,%d).\n", r, c);
+        printf("\nMap:\n");
+        for (int i = 0; i < r; ++i)
+        {
+            for (int j = 0; j < c; ++j)
+            {
+                putchar(maze[i][j] ? '#' : '.');
+                if (j + 1 < c)
+                    putchar(' ');
+            }
+            putchar('\n');
+        }
+        return;
+    }
+    else
+        print_path();
+}
+signed main()
+{
+    // run_maze();
+    run_expression();
     return 0;
 }
